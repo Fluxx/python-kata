90a313cc1c3eac95fa230c224c9080e97c9b9bb6
Jeff Pollard <jeff.pollard@gmail.com>
2011-10-31 22:24:58 -0700

    Rewrite of the data loader to extract out common logic.

--- 4_data_munging.py
+++ 4_data_munging.py
@@ -1,18 +1,32 @@
-largest_spread = 0
-day_with_largest_spread = None
+import re
 
-for line in open('weather.dat', 'r'):
-    datapoints = line.split()
+class DataLoader:
 
-    if len(datapoints) == 0:
-        continue
+    EXTRACT_CONTENT = re.compile('<pre>([^<]+)<\/pre>', re.DOTALL)
 
-    if datapoints[0].isdigit():
-        day, maximum, minimum = [ number.rstrip('*') for number in datapoints[0:3] ]
-        spread = int(maximum) - int(minimum)
-        
-        if spread > largest_spread:
-            largest_spread = spread
-            day_with_largest_spread = datapoints[0]
+    def __init__(self, filename, key_col=0, relevant_lines_regex='^\W*\d+'):
+        self.filename = filename
+        self.key_col = key_col
+        self.relevant_lines_regex = relevant_lines_regex
+    
+    def file_contents(self):
+        return open(self.filename, 'r').read()
 
-print day_with_largest_spread
+    def content_lines(self):
+        match = self.EXTRACT_CONTENT.search(self.file_contents())
+        return match.group(1).split("\n")
+    
+    def relevant_lines(self):
+        return [ line.split() for line in self.content_lines()
+                 if re.search(self.relevant_lines_regex, line) ]
+    
+    def max_diff(self, compare):
+        return sorted(self.relevant_lines(), key=compare)[0][self.key_col]
+    
+print DataLoader('weather.dat', key_col=0).max_diff(
+    lambda cols: int(cols[1].strip('*')) - int(cols[2].strip('*'))
+)
+
+print DataLoader('football.dat', key_col=1).max_diff(
+    lambda cols: abs(int(cols[6]) - int(cols[8]))
+)
